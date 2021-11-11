bb6e8c7c4ac5b5e6f551ab2105b12848f642f439
Anastasiia Shlapachenko <a.shlapachenko@chisw.com>
2021-11-11 11:28:56 +0200

    add loading indicator

---
+++ src/components/UI/LoadingIndicator/LoadingIndicator.js
@@ -0,0 +1,9 @@
+import classes from './loadingIndicator.module.scss';
+
+const LoadingIndicator = () => {
+    return (
+        <div className={classes['loading-indicator']} />
+    );
+};
+
+export default LoadingIndicator;
