a7e8bed36019de2d410479b257fed5ff1a63e184
Anastasiia Shlapachenko <a.shlapachenko@chisw.com>
2021-11-14 21:37:28 +0200

    refactor date functions as they will be reused

--- src/components/TodayWeather/TodayWeather.js
+++ src/components/TodayWeather/TodayWeather.js
@@ -1,42 +1,31 @@
 import PropTypes from 'prop-types';
 import LoadingIndicator from "../UI/LoadingIndicator/LoadingIndicator";
-import format from 'date-fns/format'
 import classes from './todayWeather.module.scss';
-import {useMemo, useRef} from "react";
+import {useMemo} from "react";
 import {capitalizeFirstLetter} from "../../utils/js";
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-
-const ICON_PATH = 'http://openweathermap.org/img/wn';
+import { getFullDate, getHourMinuteDate, getIconPath } from "../../utils/js";
 
 const TodayWeather = ({ weather }) => {
-    const todayDate = useMemo(() => weather
-        ? format(new Date(weather.current.dt), 'EEEE, MMMM do')
-        : '', [weather]);
-
     const weatherDescription = useMemo(() => weather
         ? capitalizeFirstLetter(weather.current.weather[0].description)
         : '', [weather]);
-
-    const getIconPath = icon => `${ICON_PATH}/${icon}@2x.png`;
-
-    const getSunTime = sunTime => format(new Date(sunTime), 'HH:mm');
-
-    const getHourDate = hourDate => format(new Date(hourDate), 'HH:mm');
 
     const renderHourly = () => {
         const dayHourly = weather.hourly.splice(0, 24);
 
         return dayHourly.map(hour => (
             <div className={classes['hourly-item']} key={hour.dt}>
-                <h4 className={classes['hour']}>{getHourDate(hour.dt)}</h4>
+                <h4 className={classes['hour']}>{getHourMinuteDate(hour.dt)}</h4>
                 <img src={getIconPath(hour.weather[0].icon)} alt={hour.weather[0].description} width='40' height='40' />
                 <div>{Math.round(hour.temp)}&#176;C</div>
             </div>
         ));
     }
 
-    const responsive = useRef({
+    // carousel
+    const responsive = {
         0: { items: 3 },
         420: { items: 4 },
         520: { items: 5 },
@@ -46,25 +35,22 @@ const TodayWeather = ({ weather }) => {
         920: { items: 9 },
         979: { items: 5 },
         1400: { items: 6 },
-    });
-
-
-    const renderPrevButton = ({ isDisabled }) => {
-        return <button style={{ opacity: isDisabled ? '0.5' : 1}} className={classes['carousel-prev-btn']}/>;
     };
 
-    const renderNextButton = ({ isDisabled }) => {
-        return <button style={{ opacity: isDisabled ? '0.5' : 1 }} className={classes['carousel-next-btn']}/>;
-    };
+    const renderPrevButton = ({ isDisabled }) => (
+        <button style={{ opacity: isDisabled ? '0.5' : 1}} className={classes['carousel-prev-btn']}/>
+    );
 
-
+    const renderNextButton = ({ isDisabled }) => (
+        <button style={{ opacity: isDisabled ? '0.5' : 1 }} className={classes['carousel-next-btn']}/>
+    );
 
     if (!weather) return <LoadingIndicator />
 
     return (
         <section className={classes['container']}>
             <h2 className={classes['date']}>
-                {todayDate}
+                {getFullDate(weather.current.dt)}
             </h2>
             <div className={classes['general']}>
                 <img src={getIconPath(weather.current.weather[0].icon)} alt={weatherDescription} width='70' height='70' />
@@ -74,8 +60,8 @@ const TodayWeather = ({ weather }) => {
                     <span>Feels like {Math.round(weather.current.feels_like)}&#176;C</span>
                 </div>
                 <div className={classes['secondary']}>
-                    <span>Sunrise: {getSunTime(weather.current.sunrise)}</span>
-                    <span>Sunset: {getSunTime(weather.current.sunset)}</span>
+                    <span>Sunrise: {getHourMinuteDate(weather.current.sunrise)}</span>
+                    <span>Sunset: {getHourMinuteDate(weather.current.sunset)}</span>
                 </div>
             </div>
             <div className={classes['hourly']}>
--- src/utils/js.js
+++ src/utils/js.js
@@ -1,3 +1,12 @@
-export const capitalizeFirstLetter = string => {
-    return `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
-}
+import format from 'date-fns/format';
+
+const ICON_PATH = 'http://openweathermap.org/img/wn';
+
+export const capitalizeFirstLetter = string => `${string.charAt(0).toUpperCase()}${string.slice(1)}`;
+
+export const getIconPath = icon => `${ICON_PATH}/${icon}@2x.png`;
+
+export const getFullDate = date => format(new Date(date), 'EEEE, MMMM do');
+
+export const getHourMinuteDate = date => format(new Date(date), 'HH:mm');
+
