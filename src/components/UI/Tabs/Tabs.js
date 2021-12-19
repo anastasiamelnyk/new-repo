dda37f1902d04df101c87310759165ea6a112063
Anastasiia Shlapachenko <a.shlapachenko@chisw.com>
2021-12-19 21:46:14 +0200

    add tabs styling for vertical variant

--- src/components/UI/Tabs/Tabs.js
+++ src/components/UI/Tabs/Tabs.js
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
 import classes from './Tabs.module.scss';
 import classNames from "classnames/bind";
-import {useRef, useMemo, useState, useEffect} from "react";
+import {useRef, useState, useEffect} from "react";
 
-const Tabs = ({ options, keysToDisplay, value }) => {
+const Tabs = ({ options, keysToDisplay, variant, value }) => {
     const cx = useRef(classNames.bind(classes));
+    const tabsClasses = cx.current({
+        'tabs': true,
+        'tabs-vertical': variant === 'vertical'
+    });
     const tabButtonClasses = (tabItem) => cx.current({
-        [classes['tab-button']]: true,
-        [classes['tab-button-selected']]: getTabSelected(tabItem)
+        'tab-button': true,
+        'tab-button-selected': getTabSelected(tabItem)
     });
     const [currentSelectedTab, setCurrentSelectedTab] = useState(null);
     useEffect(() => {
@@ -36,7 +40,7 @@ const Tabs = ({ options, keysToDisplay, value }) => {
     }
 
     return (
-        <ul className={classes['tabs']}>
+        <ul className={tabsClasses}>
             {renderOptions()}
         </ul>
     );
@@ -47,11 +51,13 @@ export default Tabs;
 Tabs.propTypes = {
     options: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object, PropTypes.string])),
     keysToDisplay: PropTypes.arrayOf(PropTypes.string), //needed if options are objects, selects which key info will be displayed
-    value: PropTypes.oneOfType([PropTypes.object, PropTypes.string])
+    value: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
+    variant: PropTypes.oneOf(['horizontal', 'vertical'])
 }
 
 Tabs.defaultProps = {
     options: [],
     keysToDisplay: [],
-    value: ''
+    value: '',
+    variant: 'vertical'
 }
