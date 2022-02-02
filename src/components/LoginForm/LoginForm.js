c45be8891e06db757cc5a47abcd22b54ad48a06e
Anastasiia Shlapachenko <anastasiia.shlapachenko@cd.p2h.com>
2022-02-02 17:36:22 +0200

    add inputs validation to login form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,18 +1,40 @@
 import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 import Button from "../UI/Button/Button";
 import Input from "../UI/Input/Input";
 import classes from "./LoginForm.module.scss";
 
 const LoginForm = () => {
-    const { register, handleSubmit } = useForm();
+    const schema = yup.object({
+        email: yup.string().email().required(),
+        password: yup.string().required(),
+    });
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors }
+    } = useForm({resolver: yupResolver(schema) });
+
     const login = data => {
         console.log(data);
     }
 
     return (
         <form onSubmit={handleSubmit(login)} className={classes['form']}>
-            <Input labelText="Email" register={register} inputId="email" />
-            <Input labelText="Password" register={register} inputId="password" />
+            <Input
+                labelText="Email"
+                register={register}
+                inputId="email"
+                errorText={errors.email?.message}
+            />
+            <Input
+                labelText="Password"
+                register={register}
+                inputId="password"
+                errorText={errors.password?.message}
+            />
             <Button fullWidth>
                 Login
             </Button>
