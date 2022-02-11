327b379e6c40fc454a1a542e87e6fe9be17f811c
Anastasiia Shlapachenko <anastasiia.shlapachenko@cd.p2h.com>
2022-02-11 20:24:41 +0200

    add firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {setLocationErrorAction} from "./store/locationReducer";
 import {useDispatch, useSelector} from "react-redux";
 import SavedCitiesPage from "./containers/SavedCitiesPage/SavedCitiesPage";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBJhctL5vZgOfppAXi8URb3IYEhft4Zbzk",
+    authDomain: "react-weather-app-af670.firebaseapp.com",
+    projectId: "react-weather-app-af670",
+    storageBucket: "react-weather-app-af670.appspot.com",
+    messagingSenderId: "876973096165",
+    appId: "1:876973096165:web:aa54a3187a5e61bffee80c"
+};
+
+initializeApp(firebaseConfig);
 
 
 function App() {
